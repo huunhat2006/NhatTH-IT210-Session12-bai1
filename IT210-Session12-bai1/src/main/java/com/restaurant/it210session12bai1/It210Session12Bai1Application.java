5cfd9096ca9bfd5e99fda75c6c709e2528e0448c
Huunhat123 <huunhat20052006@gmail.com>
2026-04-21 15:39:50 +0700

    Add files via upload

---
+++ IT210-Session12-bai1/src/main/java/com/restaurant/it210session12bai1/It210Session12Bai1Application.java
@@ -0,0 +1,14 @@
+package com.restaurant.it210session12bai1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It210Session12Bai1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(It210Session12Bai1Application.class, args);
+        System.out.println("Hospital Web Service is ready on Embedded Tomcat");
+    }
+
+}
---
+++ IT210-Session12-bai1/src/test/java/com/restaurant/it210session12bai1/It210Session12Bai1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.it210session12bai1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It210Session12Bai1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
